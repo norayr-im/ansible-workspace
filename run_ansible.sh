c7e125947e8a5c332109b653553e68f059b864e2
norayr <norayr.software@gmail.com>
2024-02-29 07:58:03 +0400

    Finalize

--- run_ansible.sh
+++ run_ansible.sh
@@ -1,3 +1,8 @@
-# Install and run role
+# Install role
 ansible-galaxy role install -r requirements.yml -p ./roles
+
+# Install collection
+ansible-galaxy collection install -r requirements.yml -p collections/ansible_collections/
+
+# Run
 ansible-playbook --ask-become-pass main.yml
