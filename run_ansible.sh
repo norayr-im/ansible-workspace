6630df94bcbd3ff5a4a677c3ef328d17758c136c
norayr <norayr.software@gmail.com>
2024-03-06 07:11:37 +0400

    Separate collection and role

--- run_ansible.sh
+++
@@ -1,8 +0,0 @@
-# Install role
-ansible-galaxy role install -r requirements.yml -p ./roles
-
-# Install collection
-ansible-galaxy collection install -r requirements.yml -p collections/ansible_collections/
-
-# Run
-ansible-playbook --ask-become-pass main.yml
