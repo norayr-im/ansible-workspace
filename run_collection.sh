d5d227aaeeb245af148d3aae78c1bfd49296f1a8
Norayr Mamulyan <norayr@zealous.tech>
2024-03-28 11:32:03 +0400

    Use configuration to specify collection and roles paths

--- run_collection.sh
+++ run_collection.sh
@@ -1,5 +1,5 @@
 # Install collection
-ansible-galaxy collection install -r requirements.yml -p collections/ansible_collections/
+ansible-galaxy collection install -r requirements.yml
 
 # Run
 ansible-playbook --ask-become-pass --tags "collection-test" main.yml
--- run_role.sh
+++ run_role.sh
@@ -1,5 +1,5 @@
 # Install role
-ansible-galaxy role install -r requirements.yml -p ./roles
+ansible-galaxy role install -r requirements.yml
 
 # Run
 ansible-playbook --ask-become-pass --tags "role-test" main.yml
